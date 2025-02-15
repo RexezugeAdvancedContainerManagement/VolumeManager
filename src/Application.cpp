51b19d13d63f1df234e88fff3ddc81f86dca24c6
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-02-15 06:24:46 +0000

    FEAT: Http Rest Client Implementation

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,11 +1,33 @@
 #include "Application.hpp"
 #include "gateway/HttpClient.hpp"
+#include <iostream>
+#include <json/json.h>
+#include <string>
 
 Application::Application(std::string message, IHttpClient *httpClient)
     : message(std::move(message)), httpClient(httpClient) {}
 
+Json::Value parseJson(const std::string &jsonString) {
+  Json::Value root;
+  Json::CharReaderBuilder reader;
+  std::string errs;
+
+  std::istringstream s(jsonString);
+  std::string doc;
+  s >> doc;
+
+  if (!Json::parseFromStream(reader, s, &root, &errs)) {
+    std::cerr << "Failed to parse JSON: " << errs << std::endl;
+  }
+
+  return root;
+}
+
 std::string Application::greet() const {
-  // Use the HttpClient to send a request
-  std::string httpResponse = httpClient->sendRequest("https://example.com");
+  std::string url = "http://localhost/containers/json";
+  std::string httpResponse = httpClient->sendRequest(url);
+
+  Json::Value jsonResponse = parseJson(httpResponse);
+  std::cout << "Containers: " << jsonResponse << std::endl;
   return message + " " + httpResponse;
 }
--- src/gateway/HttpClient.cpp
+++ src/gateway/HttpClient.cpp
@@ -1,6 +1,44 @@
 #include "HttpClient.hpp"
+#include <curl/curl.h>
+#include <iostream>
+#include <json/json.h>
+#include <string>
+
+size_t WriteCallback(void *contents, size_t size, size_t nmemb,
+                     std::string *s) {
+  size_t newLength = size * nmemb;
+  try {
+    s->append((char *)contents, newLength);
+  } catch (std::bad_alloc &e) {
+    return 0;
+  }
+  return newLength;
+}
 
 std::string HttpClient::sendRequest(const std::string &url,
                                     const std::string &method) const {
-  return "Hello from Http";
+  CURL *curl;
+  CURLcode res;
+  std::string response;
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_UNIX_SOCKET_PATH, "/var/run/docker.sock");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    if (method == "POST") {
+      curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    }
+
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+    }
+
+    curl_easy_cleanup(curl);
+  }
+  return response;
 }
